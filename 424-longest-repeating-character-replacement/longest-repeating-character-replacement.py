82149a9c70a387c4e91c6f9ecf993b3266f814ec
Aniket Chakraborty <aniketchakraborty808@gmail.com>
2026-08-10 21:05:48 +0530

    Time: 135 ms (75.27%) | Memory: 15.4 MB (29.59%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        seen = {}
+        left = 0
+        max_val = 0
+        output = 0
+
+        for right in range(len(s)):
+            right_char = s[right]
+            seen[right_char] = seen.get(right_char, 0) + 1
+            max_val = max(max_val, seen[right_char])
+
+            while (right - left + 1) - max_val > k:
+                seen[s[left]] -= 1
+                left += 1
+
+            output = max(output, right - left + 1)
+
+        return output
